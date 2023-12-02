1a7c8f34a599ddbe771ede4e8ebb07218f70268a
prowiz77 <148003229+prowiz77@users.noreply.github.com>
2023-12-02 02:19:28 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# 1. Install Python Dependencies
 sudo apt install python3-tk
 sudo pip install --upgrade youtube_dl
 sudo apt install ffmpeg
@@ -16,40 +15,26 @@ pip install tqdm
 
 current_directory=$(pwd)
 
-# 2. Create 'bin' Directory if it doesn't exist
 mkdir -p ~/bin/
 
-# 3. Add 'export PATH' to .bashrc if not already present
 if ! grep -q "export PATH=\$HOME/bin:\$PATH" ~/.bashrc; then
     echo 'export PATH="$HOME/bin:$PATH"' >> ~/.bashrc
 fi
 
-# 4. Source .bashrc to apply changes immediately
 source ~/.bashrc
 
 if [ -d images ]; then
     mv images ~/bin/
 else
-    echo "Der Ordner 'images' existiert nicht im aktuellen Verzeichnis."
+    echo "Folder 'images' does not exist in current DIR."
 fi
 
 if [ -f CeNtis.py ]; then
     mv CeNtis.py ~/bin/
 else
-    echo "Die Datei CeNtis.py existiert nicht im aktuellen Verzeichnis."
+    echo "File CeNtis.py does not exist in current DIR."
 fi
 
-# 5. Navigate to the Home Directory
-#cd ~/bin/
-
-# 6. Git Clone Repository
-#git clone https://github.com/prowiz77/CeNtis.git
-
-# 7. Copy CeNtis.py to 'bin' Directory
-#cp ~/bin/CeNtis/CeNtis.py ~/bin/
-#cp -r ~/bin/CeNtis/images/ ~/bin/
-
-# 8. Make CeNtis.py Executable
 chmod +x ~/bin/CeNtis.py
 
 echo "Setup completed successfully."
